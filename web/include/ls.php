9c479a1bb09b777eeb2edc00d4ab5754b3d8c306
fidels80 <63100124+fidels80@users.noreply.github.com>
2022-02-17 12:17:39 +0100

    1.1.4

--- web/include/ls.php
+++ web/include/ls.php
@@ -14,7 +14,9 @@ class ls
         $ext = $ini_array['Parametri']['estensione'];
        // echo  str_replace('include','',__DIR__).$ini_array['percorsi']['oripath'];
          echo  (str_replace('include','',__DIR__).$ini_array['percorsi']['oripath']);
-        if (!is_dir(str_replace('include','',__DIR__).$ini_array['percorsi']['oripath'])) {
+        if (!is_dir('/app/web/toelab/')
+        //(str_replace('include','',__DIR__).$ini_array['percorsi']['oripath'])
+        ) {
 
             echo "<H1>attenzione la directory di origine  non esiste controllare il config.ini voce oripath</h1>";
         }
